3a80075a476c4e5efc8fef1c332caf5dbfacc8ec
Emmanuel Guerra <emmanuelnypd@gmail.com>
2021-12-28 21:46:38 -0400

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Emmanuel Guerra on 28/12/21.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
